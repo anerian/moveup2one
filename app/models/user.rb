3b8e02ff0272d11bd55a027ced8aee4ff627a89e
Min Kim <minho.kim@gmail.com>
2009-06-24 18:27:54 -0400

    add user subscription association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   has_many :ranked_lists, :through => :rankings
   has_many :friendships
   has_many :friends, :through => :friendships
+  has_many :payments
+  
+  has_one :subscription, :dependent => :destroy
 end
